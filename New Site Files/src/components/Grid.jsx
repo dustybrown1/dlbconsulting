a42d556f3b383ed20a8b51a0cabdc086f2940235
dustybrown1 <dustylynnbrown@gmail.com>
2026-05-12 10:41:37 -0500

    Delete New Site Files directory

--- New Site Files/src/components/Grid.jsx
+++
@@ -1,5 +0,0 @@
-export default function Grid() {
-  return (
-    <div style={{ position: "fixed", inset: 0, pointerEvents: "none", zIndex: 0, backgroundImage: "linear-gradient(rgba(0,180,255,0.03) 1px, transparent 1px), linear-gradient(90deg, rgba(0,180,255,0.03) 1px, transparent 1px)", backgroundSize: "60px 60px" }} />
-  );
-}
